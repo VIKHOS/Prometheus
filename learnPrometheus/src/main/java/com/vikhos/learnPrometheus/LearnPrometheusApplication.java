613a85014b54539589cd8a7208f3b2178e1d0566
Vikesh Hosanee <vikesh.hosanee@mcb.mu>
2021-03-28 18:30:36 +0400

    Create the Prometheus project

---
+++ learnPrometheus/src/main/java/com/vikhos/learnPrometheus/LearnPrometheusApplication.java
@@ -0,0 +1,13 @@
+package com.vikhos.learnPrometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnPrometheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnPrometheusApplication.class, args);
+	}
+
+}
